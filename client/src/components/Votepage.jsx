3eaed37ae6d50e409101060c0d4443280e64088b
MuhiaKevin <archerkreiger@gmail.com>
2020-12-19 11:48:18 +0300

    feat(component): prevent user from voting twice

--- client/src/components/Votepage.jsx
+++ client/src/components/Votepage.jsx
@@ -2,26 +2,35 @@ import React, { useEffect, useState } from 'react';
 // https://stackoverflow.com/questions/48846289/why-is-my-react-component-is-rendering-twice
 // https://jasonwatmore.com/post/2020/02/01/react-fetch-http-post-request-examples
 // https://www.codementor.io/@idreesibraheem/how-to-deploy-a-minimal-docker-app-to-heroku-136rp9223y
+// https://medium.com/mozilla-club-bbsr/dockerizing-a-mern-stack-web-application-ebf78babf136
+
 
 const Votepage = props => {
-    const [vote, setVotes] = useState({ vote: {}, isFetching: true, hasVoted : false })
+    const [vote, setVotes] = useState({ vote: {}, isFetching: true, hasVoted: false })
     const baseurl = "http://localhost:5000/api/v1/poll"
+
 
     function upVote(pollid, optionIndex) {
         const requestOptions = {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ optionindex: optionIndex })
+            body: JSON.stringify({ optionindex: optionIndex, pollid: pollid })
         }
 
-        fetch(`${baseurl}/${pollid}/vote`, requestOptions)
+
+        fetch(`${baseurl}/vote`, requestOptions)
             .then(async response => {
-                const data = await response.json();
-                setVotes({ vote: data, isFetching: false, hasVoted : true })
+                const data = await response.json();               
+                localStorage.setItem('hasVoted', true);
+
+
+                setVotes({ vote: data, isFetching: false, hasVoted: true })
             })
             .catch(error => {
                 console.error('There was an error!', error);
             });
+
+
     }
 
     useEffect(() => {
@@ -30,42 +39,52 @@ const Votepage = props => {
             const url = `${baseurl}/stats/${voteid}`
             const api_call = await fetch(url);
             const response = await api_call.json()
-            setVotes({ vote: response, isFetching: false })
-        }
-        getPollData()
+
+            let savedVote = localStorage.getItem('hasVoted');
+            let voted = false;
+
+            if (savedVote === null) {
+                localStorage.setItem('hasVoted', voted);
+            } else {
+                voted = JSON.parse(localStorage.getItem('hasVoted'))
+            }
+
+            setVotes({ vote: response, isFetching: false, hasVoted: voted })
+}
+getPollData()
 
     }, [])
 
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-                <div className="container">
-                    <a className="navbar-brand" href="http://localhost:3000/">Poll App</a>
-                </div>
-            </nav>
-            <div className="d-flex justify-content-center" style={{ marginTop: "10px" }}>
-                {vote.isFetching ? (<div>Loading...</div>) : (
-                    <div className="card text-black bg-light mb-3" style={{ maxWidth: "18rem;" }}>
-                        <div className="card-header">{vote.vote.question}</div>
-                        <div className="card-body">
-                            {vote.vote.options.map((voteitem, index) => {
-                                return (
-                                    <div key={index}>
-                                        <p>{voteitem.description} <strong>{voteitem.votes}</strong> </p>
-                                        <button onClick={() => upVote(vote.vote._id, index)} className="btn btn btn-primary" disabled={vote.hasVoted}>Vote </button>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                        <p style={{ marginTop: "10px", marginLeft: "100px" }}>Total Votes <strong>{vote.vote.total_votes}</strong></p>
-
-                    </div>
-                )
-                }
+return (
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+            <div className="container">
+                <a className="navbar-brand" href="http://localhost:3000/">Poll App</a>
             </div>
-        </div>
+        </nav>
+        <div className="d-flex justify-content-center" style={{ marginTop: "10px" }}>
+            {vote.isFetching ? (<div>Loading...</div>) : (
+                <div className="card text-black bg-light mb-3" style={{ maxWidth: "18rem;" }}>
+                    <div className="card-header"><strong>{vote.vote.question}</strong></div>
+                    <div className="card-body">
+                        {vote.vote.options.map((voteitem, index) => {
+                            return (
+                                <div key={index}>
+                                    <p>{voteitem.description} <strong>{voteitem.votes}</strong> </p>
+                                    <button onClick={() => upVote(vote.vote._id, index)} className="btn btn btn-primary" disabled={vote.hasVoted}>Vote </button>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <p style={{ marginTop: "10px", marginLeft: "100px" }}>Total Votes <strong>{vote.vote.total_votes}</strong></p>
 
-    );
+                </div>
+            )
+            }
+        </div>
+    </div>
+
+);
 };
 
 export default Votepage;
